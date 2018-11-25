f534c37e758670869bb20ae091ce44395199b641
raffithemaster <45330163+raffithemaster@users.noreply.github.com>
2018-11-25 11:53:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function start() {
+    if(document.body.classList.contains("test")) {
+        document.body.classList.remove("test")
+    } else {
+        document.body.classList.add("test");
+
+    }
+}
